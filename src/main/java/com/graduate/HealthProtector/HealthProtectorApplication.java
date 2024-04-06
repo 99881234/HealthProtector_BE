7ccc8f5985e954538d2ca3c2462f7fc3c4bbf667
hoyeon <aviationlovers@naver.com>
2024-04-06 23:59:28 +0900

    chore : swagger 설정

---
+++ src/main/java/com/graduate/HealthProtector/HealthProtectorApplication.java
@@ -0,0 +1,13 @@
+package com.graduate.HealthProtector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthProtectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthProtectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graudate/HealthProtector/HealthProtectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graudate.HealthProtector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthProtectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
