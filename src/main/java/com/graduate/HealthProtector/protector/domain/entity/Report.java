2cec3dfca7e5f78cb85f01e8a513e4c85887c00c
hoyeon <aviationlovers@naver.com>
2024-05-02 23:57:49 +0900

    feat : 건강리포트 구현

---
+++ src/main/java/com/graduate/HealthProtector/protector/domain/entity/Report.java
@@ -0,0 +1,21 @@
+package com.graduate.HealthProtector.protector.domain.entity;
+
+import com.graduate.HealthProtector.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Report {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_id")
+    private Long id;
+    private String content;
+    @Enumerated(EnumType.STRING)
+    private State state;
+    @OneToMany(cascade = )
+
+}
