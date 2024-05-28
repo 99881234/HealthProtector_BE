378bbae0a8774101b0cbd36bda878e7ba693f626
hoyeon <aviationlovers@naver.com>
2024-05-29 03:22:39 +0900

    feat : 건강리포트 기능 개발

--- src/main/java/com/graduate/HealthProtector/protector/api/controller/ReportGeneratorController.java
+++ src/main/java/com/graduate/HealthProtector/protector/api/controller/ReportGeneratorController.java
@@ -1,27 +1,23 @@
 package com.graduate.HealthProtector.protector.api.controller;
 
+import com.graduate.HealthProtector.global.template.BaseResponse;
 import com.graduate.HealthProtector.protector.application.ReportGeneratorService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/chat")
 public class ReportGeneratorController {
     private final ReportGeneratorService reportGeneratorService;
 
-    public ReportGeneratorController(ReportGeneratorService reportGeneratorService) {
+    public ReportGeneratorController(ReportGeneratorService reportGeneratorService, RestTemplate restTemplate) {
         this.reportGeneratorService = reportGeneratorService;
     }
 
-//    @Operation(summary = "건강 리포트 생성", description = "건강 리포트를 생성합니다.")
-//    @PostMapping(value = "/create")
-//    public ResponseEntity<?> create (){
-//        // 건강 리포트를 생성하기 위해서 필요한 점
-//        // 받아와서 세팅해야될게 뭔지를 생각
-//
-//
-//    }
+    @GetMapping("/createReport")
+    @Operation(summary = "건강 리포트 생성", description = "건강 리포트를 생성합니다.")
+    public BaseResponse<?> createReport(@RequestParam(name = "loginId") String loginId, @RequestParam(name = "message") String message) {
+        return reportGeneratorService.getChatResponse(loginId, message);
+    }
 }
--- src/main/java/com/graduate/HealthProtector/protector/domain/entity/Report.java
+++ src/main/java/com/graduate/HealthProtector/protector/domain/entity/Report.java
@@ -2,17 +2,42 @@ package com.graduate.HealthProtector.protector.domain.entity;
 
 import com.graduate.HealthProtector.user.domain.entity.User;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 @Entity
+@Data
 @NoArgsConstructor
-@Getter
 public class Report {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "report_id")
-    private Long id;
-    private String content;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userMessage;
+    private String botResponse;
+    private LocalDateTime createDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createDate = LocalDateTime.now();
+    }
+
+    @Builder
+    public Report(String userMessage, String botResponse, User user) {
+        this.userMessage = userMessage;
+        this.botResponse = botResponse;
+        this.user = user;
+    }
 }
+
+
+
+
+
