952a1688660a7276221379d3bcd52e68907e18a5
hoyeon <aviationlovers@naver.com>
2024-11-26 23:19:12 +0900

    Chore: 불필요 코드 삭제

--- src/main/java/com/graduate/HealthProtector/protector/api/controller/ReportGeneratorController.java
+++ src/main/java/com/graduate/HealthProtector/protector/api/controller/ReportGeneratorController.java
@@ -2,17 +2,9 @@ package com.graduate.HealthProtector.protector.api.controller;
 
 import com.graduate.HealthProtector.global.template.BaseResponse;
 import com.graduate.HealthProtector.protector.application.ReportGeneratorService;
-import com.graduate.HealthProtector.user.domain.entity.User;
 import com.graduate.HealthProtector.user.domain.repository.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/chat")
